daaa0809aee439bf9af46ee90f1beae90aca6668
Julio <j.jaimes.rodriguez@gmail.com>
2020-07-15 17:20:46 -0400

    mongodb

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const conectarDB = async () => {
         await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser : true,
             useUnifiedTopology : true,
-            unseFindAndModify : false
+            unseFindAndModify : true
         });
         console.log("data base coneecte successful")
     } catch (error) {
